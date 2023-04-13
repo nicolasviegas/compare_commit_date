1d8a508061e7d95125d57aa0b871abbfb4cdc230
nicolasviegas <nicolasviegas38@gmail.com>
2023-04-13 13:59:04 -0300

    Este funciona, saca las fechas de los codigos en el excel

--- compare_commit_date.py
+++ compare_commit_date.py
@@ -19,7 +19,7 @@ import openpyxl
 import time
 import os
 import shutil
-#from pyautogui import *
+# from pyautogui import *
 # import pyautogui
 # import pyperclip
 # #import keyboard
@@ -32,16 +32,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-
-
 # PATH ES LA DIRECCION DONDE SE ENCUENTRA EL EXCEL
-#PARENT DIR ES LA DIR DONDE SE VA A EJECUTAR EL SCRIPT Y DONDE SE VAN A CREAR LAS CARPETAS DE LOS CASOS
+# PARENT DIR ES LA DIR DONDE SE VA A EJECUTAR EL SCRIPT Y DONDE SE VAN A CREAR LAS CARPETAS DE LOS CASOS
 path = "/Users/nicolasviegas/Documents/full_screenshot.py/examples/excel.xlsx"
-#parent_dir = "C:\\Users\\nviegas001\\python-scripts\\"
-#download_dir = "C:\\Users\\nviegas001\\Downloads\\"
+# parent_dir = "C:\\Users\\nviegas001\\python-scripts\\"
+# download_dir = "C:\\Users\\nviegas001\\Downloads\\"
 github_list = []
-
-
 
 
 def code_list_to_analyze():
@@ -49,7 +45,7 @@ def code_list_to_analyze():
 
     sheet_obj = wb_obj.active
 
-    cell_obj = sheet_obj.cell(row = 1, column = 1)
+    cell_obj = sheet_obj.cell(row=1, column=1)
     max_col = sheet_obj.max_column
     max_r = sheet_obj.max_row
 
@@ -63,15 +59,11 @@ def code_list_to_analyze():
         github_list.append(str(link))
 
 
-def open_individual_links(chrome, link):
+def obtain_date_commit(chrome, link):
     chrome.get(link)
-    chrome.find_element(By.ID, "login_field").send_keys("nicolasviegas")
-    chrome.find_element(By.ID, "password").send_keys("EXWfS2J4#@cn")
-    chrome.find_element(By.NAME, 'commit').click()
     commit = chrome.find_element(By.XPATH, "//relative-time[@class='no-wrap']").get_attribute("title")
 
     print(commit)
-
 
 
 def open_list_links():
@@ -81,18 +73,25 @@ def open_list_links():
     chrome_options.add_experimental_option('detach', True)
 
     chrome = webdriver.Chrome(executable_path=r"/Users/nicolasviegas/Documents/webdriver\chromedriver_mac64",
-                          options=chrome_options)
+                              options=chrome_options)
+
+    chrome.get(github_list[0])
+    chrome.find_element(By.ID, "login_field").send_keys("nicolasviegas")
+    chrome.find_element(By.ID, "password").send_keys("EXWfS2J4#@cn")
+    chrome.find_element(By.NAME, 'commit').click()
 
     for i in github_list:
         if i != 'None':
+            obtain_date_commit(chrome, i)
 
-            open_individual_links(chrome, i)
-
-
-code_list_to_analyze()
-
-open_list_links()
+    chrome.quit()
 
 
+def main():
+    code_list_to_analyze()
+
+    open_list_links()
 
 
+if __name__ == "__main__":
+    main()
